30eb61e0f95276c9265587cf82a1a63401218a42
ayushsutariya <ayushsutariya09@gmail.com>
2022-04-04 20:45:43 +0530

    map-filter-reduce Array_funtion

---
+++ src/container/Array_Function.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ArrayFunction = () => {
+   const Funct = ['Ayush', 101, 'Dhruv', 102];
+    
+    return (
+        <div>
+        
+        </div>
+    );
+}
+
+export default ArrayFunction;
