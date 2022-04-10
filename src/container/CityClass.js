9b64bc60639e9b9a0e224a75b55a6e1edde5c34b
ayushsutariya <ayushsutariya09@gmail.com>
2022-04-10 12:03:13 +0530

    city_class_completed

--- src/container/CityClass.js
+++ src/container/CityClass.js
@@ -1,30 +1,60 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 
 export default class CityClass extends Component {
   constructor(props) {
     super(props);
-    this.state = [
-      {
-        statename: "Gujarat",
-        cityname: "Surat",
-        id: 101,
-        tempertaure: "35 degree",
-      },
-      {
-        statename: "Gujarat",
-        cityname: "Rajkot",
-        id: 102,
-        tempertaure: "30 degree",
-      },
-      {
-        statename: "Maharastra",
-        cityname: "Pune",
-        id: 102,
-        tempertaure: "33 degree",
-      },
-    ];
+    this.state = {
+      stn : [
+        {
+          statename: 'Maharastra',
+              id: 101,
+              cityname: 'Pune',
+              tempertaure: '33 degree',
+              
+        },
+        {
+          statename: 'Gujarat',
+              id: 102,
+              cityname: 'Surat',
+              tempertaure: '36 degree',
+              
+        },
+        {
+          statename: 'Rajasthan',
+              id: 103,
+              cityname: 'Jaipur',
+              tempertaure: '40 degree',
+              
+        },
+      ]
+    }
     this.state1 = [10, 20, 30, 40];
   }
+
+  changeArray = (id) => {
+    let newArray =this.state.stn.map((a) => {
+      if(a.id === 101){
+        let n = {
+          statename: 'Tamilnadu',
+          id: 104,
+          cityname: 'Chennai',
+          tempertaure: '35 degree'
+        } 
+        return n;
+      } else if(a.id === 102){
+        let n ={
+          statename: 'Madhya Pradesh',
+          id: 105,
+          cityname: 'Indore',
+          tempertaure: '38 degree'
+        }
+        return n;
+      } else{
+        return a;
+      }
+    })
+    this.setState({stn: newArray})
+      } 
 
   render() {
     return (
@@ -34,7 +64,7 @@ export default class CityClass extends Component {
             <h3>City Class</h3>
           </td>
         </th>
-        <tr colSpan={this.state.length}>
+        <tr colSpan={this.state.stn.length}>
           <td>
             <h3>Object</h3>
           </td>
@@ -43,15 +73,15 @@ export default class CityClass extends Component {
           <td>
             <h3>State Name : </h3>
           </td>
-          {this.state.map((a) => {
-            return <td>{a.statename}</td>;
+          {this.state.stn.map((a) => {
+            return <td><button onClick={() => this.changeArray(a.id)}>{a.statename}</button></td>;
           })}
         </tr>
         <tr>
           <td>
             <h3>ID : </h3>
           </td>
-          {this.state.map((a) => {
+          {this.state.stn.map((a) => {
             return <td>{a.id}</td>;
           })}
         </tr>
@@ -59,7 +89,7 @@ export default class CityClass extends Component {
           <td>
             <h3>City Name : </h3>
           </td>
-          {this.state.map((a) => {
+          {this.state.stn.map((a) => {
             return <td>{a.cityname}</td>;
           })}
         </tr>
@@ -67,11 +97,11 @@ export default class CityClass extends Component {
           <td>
             <h3>Tempertaure : </h3>
           </td>
-          {this.state.map((a) => {
+          {this.state.stn.map((a) => {
             return <td>{a.tempertaure}</td>;
           })}
         </tr>
-        <tr colSpan={this.state.length}>
+        <tr colSpan={this.state.stn.length}>
           <td>
             <h3>Array</h3>
           </td>
