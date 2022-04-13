f11258459e1cba310a589f74022757c8a463bafa
ayushsutariya <ayushsutariya09@gmail.com>
2022-04-13 08:24:19 +0530

    clockfunction and clockclass completed

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import ArrayFunction from './container/Array_Function';
 import StateClass from './container/StateClass';
 import CityClass from './container/CityClass';
 import ClockClass from './container/ClockClass';
+import ClockFunction from './container/ClockFunction';
+// import Counter from './container/Counter';
 
 function App() {
   return (
     <>
     <ClockClass/>
-    
+    <ClockFunction/>
+    {/* <Counter/> */}
     <MapFilterReduce/>
     <ArrayFunction/>
     <StateClass/>
--- src/container/ClockClass.js
+++ src/container/ClockClass.js
@@ -2,23 +2,42 @@ import React, { Component } from 'react'
 
 export default class ClockClass extends Component {
 
-    constructor(props) {
-        super(props);
-        console.log("1 Constructor");
+  constructor(props) {
+    super(props);
+    this.state = {
+      time : new Date()
     }
-    
-    componentDidMount = () => {
-        console.log("3 ComponentDidMount")
-    }
-    
-    componentDidUpdate = () => {
-        console.log("ComponentDidUpdate");
-    }
+  }
+
+  tick = () => {
+    this.setState({
+      time : new Date()
+    })
+  } 
+
+  componentDidMount = () => {
+   this.timerid = setInterval(() => {
+      this.tick()
+    }, 1000);
+  }
+
+  componentDidUpdate = () => {
+    console.log("ComponentDidUpdate")
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.timerid)
+  }
 
   render() {
-      console.log("2 render")
     return (
-      <div>Clock</div>
+      <table>
+        <tr>
+        <td><h2>Time(Class) : </h2></td>
+          <td><h2>{this.state.time.toLocaleTimeString()}</h2></td>
+        </tr>
+      </table>
     )
   }
 }
+
